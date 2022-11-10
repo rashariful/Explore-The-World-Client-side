e8f5a36816a621ac15fef19cf468d6b4aaff71a2
md shariful islam <raislam.shariful@gmail.com>
2022-11-10 10:11:46 +0600

    jwt token verify function created

--- src/Components/Pages/Home/UserReviews/UserReviews.js
+++ src/Components/Pages/Home/UserReviews/UserReviews.js
@@ -2,13 +2,22 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../../Context/UserContext';
 
 const UserReviews = () => {
-    const { user } = useContext(AuthContext);
+    const { user, logoutUser } = useContext(AuthContext);
     const [orders, setOrders] = useState([])
     console.log(orders.length);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/userreview?email=${user?.email}`)
-            .then(res => res.json())
+        fetch(`http://localhost:5000/userreview?email=${user?.email}`,{
+            headers:{
+                authorization: `Bearer${localStorage.getItem('token')}`
+            }
+        })
+            .then(res => {
+                if(res.status === 401 || res.status === 403){
+                    logoutUser()
+                }
+                return res.json()
+            })
             .then(data => setOrders(data.data))
     }, [user?.email])
     return (
